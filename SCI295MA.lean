3665fd5a8aa905d2cfa4accae77f4f9eb601ca7d
Student <student@A4583--Laptop-036.local>
2026-01-29 15:01:48 -1000

    first proof

--- SCI295MA.lean
+++ SCI295MA.lean
@@ -3,3 +3,10 @@ import Mathlib.Data.Nat.Basic
 theorem zeroPlusNisN (n : ℕ) : 0 + n =  n := by {
   exact Nat.zero_add n
 }
+
+theorem transitivity (a b c : ℕ) : (a=b) ∧ (b=c) → (a=c) := by {
+  intro h
+  rcases h with ⟨ hl,hr ⟩
+  rw [hl]
+  rw [hr]
+}
